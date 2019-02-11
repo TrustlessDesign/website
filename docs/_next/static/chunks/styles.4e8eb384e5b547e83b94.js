b804480b771402b56e1d0f3f71dbe5e78c22c5f7
Kuzirashi <kuzirashi@interia.pl>
2019-02-11 21:12:20 +0100

    Upgrade Next to v8.

---
+++ docs/_next/static/chunks/styles.4e8eb384e5b547e83b94.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{EvPg:function(n,w,o){}}]);
--- docs/_next/static/chunks/styles.c7ccf226b7837e989828.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{226:function(n,w,o){}}]);
